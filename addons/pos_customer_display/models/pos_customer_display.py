fe0c4ac6d378ceca6009c64572210f7c282d641b
Andrey Troitskiy <troizky@mail.ru>
2018-12-07 17:17:09 +0300

    Fix for wide char strings

--- addons/pos_customer_display/models/pos_customer_display.py
+++ addons/pos_customer_display/models/pos_customer_display.py
@@ -13,19 +13,19 @@ class PosConfig(models.Model):
         string='Line Length of the Customer Display', default=20,
         help="Length of the LEDs lines of the customer display")
     customer_display_msg_next_l1 = fields.Char(
-        string="Next Customer (top line)", default=_("Welcome!"),
+        string="Next Customer (top line)", default="Добро пожаловать!",
         help="Top line of the message on the customer display which is "
         "displayed after starting POS and also after validation of an order")
     customer_display_msg_next_l2 = fields.Char(
-        string="Next Customer (bottom line)", default=_("Point of Sale Open"),
+        string="Next Customer (bottom line)", default="Точка продаж открыта",
         help="Bottom line of the message on the customer display which is "
         "displayed after starting POS and also after validation of an order")
     customer_display_msg_closed_l1 = fields.Char(
-        string="POS Closed (top line)", default=_("Point of Sale Closed"),
+        string="POS Closed (top line)", default="Точка продаж закрыта",
         help="Top line of the message on the customer display which "
         "is displayed when POS is closed")
     customer_display_msg_closed_l2 = fields.Char(
-        string="POS Closed (bottom line)", default=_("See you soon!"),
+        string="POS Closed (bottom line)", default="До скорой встречи!",
         help="Bottom line of the message on the customer display which "
         "is displayed when POS is closed")
 
@@ -48,9 +48,9 @@ class PosConfig(models.Model):
                 self.customer_display_msg_closed_l2,
             }
             for field, msg in to_check.iteritems():
-                if msg and len(msg) > maxsize:
+                if msg and len(msg.decode('utf8')) > maxsize:
                     raise ValidationError(_(
                         "The message for customer display '%s' is too "
                         "long: it has %d chars whereas the maximum "
                         "is %d chars.")
-                        % (field, len(msg), maxsize))
+                        % (field, len(msg.decode('utf8')), maxsize))
